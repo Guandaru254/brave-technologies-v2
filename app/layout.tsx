5e68e75d21d749436e436d607e036d617cfc99ff
Guandaru254 <leroykim82@gmail.com>
2026-08-14 15:50:39 +0300

    feat: integrate local business schema markup into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,47 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const schema = {
+    '@context': 'https://schema.org',
+    '@type': 'LocalBusiness',
+    name: 'Brave Technologies Ltd',
+    image: 'https://www.bravetechnologies.co.ke/favicon.ico',
+    '@id': 'https://www.bravetechnologies.co.ke',
+    url: 'https://www.bravetechnologies.co.ke',
+    telephone: '+254793595290',
+    email: 'info@bravetechnologies.co.ke',
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Tom Mboya Street',
+      addressLocality: 'Nairobi',
+      addressCountry: 'KE',
+    },
+    geo: {
+      '@type': 'GeoCoordinates',
+      latitude: -1.286389,
+      longitude: 36.817223,
+    },
+    openingHoursSpecification: {
+      '@type': 'OpeningHoursSpecification',
+      dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      opens: '09:00',
+      closes: '17:00',
+    },
+    sameAs: [
+      'https://www.instagram.com/bravetechnologies.co.ke',
+      'https://www.facebook.com',
+      'https://www.linkedin.com',
+    ],
+  };
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      </head>
       <body className="antialiased">
         <Navbar />
         {children}
